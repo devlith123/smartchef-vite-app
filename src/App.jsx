a4d2785e142d6ff1c16d2f8063fd5b75466c0e9a
Deva <devareddysrt@gmail.com>
2025-10-21 22:02:53 +0530

    Third Commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,13 @@ import {
 // --- Firebase Configuration ---
 // IMPORTANT: Replace this with your own Firebase project configuration.
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBftMuoj3qY5uE36I_x5WtBX4JAh1wFZgc",
+  authDomain: "smartchefai-78cae.firebaseapp.com",
+  projectId: "smartchefai-78cae",
+  storageBucket: "smartchefai-78cae.firebasestorage.app",
+  messagingSenderId: "279030560133",
+  appId: "1:279030560133:web:02577656535c3d919180ec",
+  measurementId: "G-DRZ41NQ6J4"
 };
 
 // --- Initialize Firebase ---
